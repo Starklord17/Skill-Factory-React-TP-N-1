9fd52deb88b71a29ca1560445022d4c245b74b1b
Max_S <91400948+Starklord17@users.noreply.github.com>
2022-08-13 14:37:29 -0300

    Update formulario.js

--- form/formulario.js
+++ form/formulario.js
@@ -3,14 +3,14 @@ const formulario = document.getElementById('form')
 
 formulario.addEventListener('submit',submitForm)
 
-/*Si clicamos en el botón del sol, borrarémos la clase css dark-mode del div 
+/* El botón del solo borrará la clase css dark-mode del div 
 con id page y se aplicará el estilo active al sol*/
 document.getElementById('id-sun').onclick = function(){
     document.getElementById('page').classList.remove('dark-mode')
     document.getElementById('id-moon').classList.remove('active')
     this.classList.add('active')
 }
-  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
+  /* El botón de la luna añadira la clase css dark-mode del div 
   con id page y se aplicará el estilo active a la luna*/
 document.getElementById('id-moon').onclick = function(){
     document.getElementById('page').classList.add('dark-mode')
@@ -18,7 +18,6 @@ document.getElementById('id-moon').onclick = function(){
     this.classList.add('active')
 }
 
-//Preguntar a Fabi ------- objeto vacio? con datos? RAAAAAARO
 function submitForm (e) {
     e.preventDefault()
     const data = new FormData(this)
